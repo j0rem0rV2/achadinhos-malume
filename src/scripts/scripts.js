619cfe78cf9d2c633d69c3e703bee1fac68f3e95
j0rem0rV2 <phantonJ0rem0rV2@proton.me>
2024-07-23 23:14:52 -0100

    add hide butons

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,21 @@
+
 AOS.init();
 
-const promoDate = new Date("Jul 22, 2024 00:00:00");
-const promoStamp = promoDate.getTime();
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.hero__button');
+    const sections = document.querySelectorAll('.store__home');
 
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+
+            sections.forEach(section => {
+                if (section.classList.contains(`store__home--${target}`)) {
+                    section.classList.add('active');
+                } else {
+                    section.classList.remove('active');
+                }
+            });
+        });
+    });
+});
